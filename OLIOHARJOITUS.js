b92c17869d1376c5b2b13e0e4d2860c2a083669a
CaritaNi <93704532+CaritaNi@users.noreply.github.com>
2021-12-29 22:15:16 +0200

    Olioharjoituksia

---
+++ OLIOHARJOITUS.js
@@ -0,0 +1,60 @@
+/*
+let fname = "Reima";
+
+testi(fname);
+
+function testi(n){
+    n = "Liisa";
+}
+
+console.log(fname);
+*/
+
+/*let person = {
+    fname: "Reima",
+    lname: "Riihimäki",
+    age: 22
+}*/
+
+//person.length = 169;
+//person.age = 25;
+
+//console.log(person.fname);
+/*
+let persons = [
+    {
+    fname: "Reima",
+    lname: "Riihimäki",
+    age: 22
+    },
+    {
+        fname: "Lasse",
+        lname: "Haverinen",
+        age:28
+    },
+];
+*/
+//console.log(persons[1].lname);
+
+//let copyPerson = person;
+// copyPerson.fname= "Reiska";
+//console.log(person);
+
+/*
+testi(person);
+
+console.log(person.age);
+
+function testi(p){
+    p.age = 50;
+} */
+/*
+function testi(p){
+    p={
+        fname: "Timo",
+        lname: "Timoteus",
+        age: 12
+    }
+}*/
+
+
